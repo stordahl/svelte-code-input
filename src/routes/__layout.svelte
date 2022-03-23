fdef0280eecd1c4e03a3902309930c2e4a13e844
stordahl <jacob.stordahl@stylitics.com>
2022-03-23 00:28:22 -0500

    Setup docs site

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,56 @@
+<script lang=ts context=module>
+   
+</script>
+<script lang=ts>
+  import "./base.css"
+</script>
+
+<header>
+  <div>
+    <h1>svelte-code-input</h1>
+  </div>
+  <div id="external">
+    <a href="https://npmjs.org">
+      <img src="images/npm.png" alt="View on npm" aria-label="View on npm">
+    </a>
+    <a href="https://github.com/stordahl/svelte-code-input">
+      <img src="images/github.png" alt="View on GitHub" aria-label="View on GitHub">
+    </a>
+  </div>
+</header>
+<main>
+  <slot/>
+</main>
+<footer>
+  <span>Made with ❤️ by <a href="https://stordahl.dev">Jacob Stordahl</a> | 2022</span>
+</footer>
+
+<style>
+  header {
+    background-color: var(--black);
+    color: var(--white);
+    padding: 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  #external {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+  }
+  header img {
+    max-width: 30px;
+  }
+  main {
+    margin: 0 2rem;
+  }
+  footer {
+    padding: 2rem;
+    text-align: center;
+    font-size: 1.2rem;
+  }
+  footer a {
+    color: white;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,9 +2,11 @@
   import CodeEditor from "$lib/CodeEditor.svelte"
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
 <CodeEditor 
   name="A Code Input"
+  innerPadding="2rem"
+  lineCountBg="#02111B"
+  lineCountColor="#fcfcfc"
+  editorBg="#fcfcfc"
+  editorColor="#2b2930"
 />
