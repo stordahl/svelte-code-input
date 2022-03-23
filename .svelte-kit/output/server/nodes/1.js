b25f0f1be707f3f9b294e8705100522c8d0b4656
stordahl <jacob.stordahl@stylitics.com>
2022-03-23 18:08:41 -0500

    removing .svelte-kit from git

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,6 +0,0 @@
-import * as module from '../entries/fallbacks/error.svelte.js';
-
-export { module };
-export const entry = 'error.svelte-7b121b23.js';
-export const js = ["error.svelte-7b121b23.js","chunks/vendor-c78b13c5.js"];
-export const css = [];
--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,6 +0,0 @@
-import * as module from '../entries/pages/index.svelte.js';
-
-export { module };
-export const entry = 'pages/index.svelte-96f9d6b5.js';
-export const js = ["pages/index.svelte-96f9d6b5.js","chunks/vendor-c78b13c5.js"];
-export const css = ["assets/pages/index.svelte-3d80129e.css"];
